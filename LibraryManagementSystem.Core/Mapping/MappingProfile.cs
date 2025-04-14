520af6a5394cf8c4ccfbc7a166895dff9b0a08c8
khaledahmed2 <khaled.ahmed@appenza-studio.com>
2025-04-14 02:41:27 +0200

    Add-Delete-GetAllLibrarins

--- LibraryManagementSystem.Core/Mapping/MappingProfile.cs
+++ LibraryManagementSystem.Core/Mapping/MappingProfile.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using LibraryManagementSystem.Core.DTOs.Librarian;
 using LibraryManagementSystem.Core.DTOs.User;
 using LibraryManagementSystem.Core.Entities.User;
 
@@ -13,7 +14,21 @@ namespace LibraryManagementSystem.Core.Mapping
     {
         public MappingProfile()
         {
+            MapUser();
+            MapLibrarian();
+        }
+
+        private void MapUser()
+        {
             CreateMap<RegisterDto, AppUser>();
+        }
+
+        private void MapLibrarian()
+        {
+            CreateMap<CreateLibrarianDto, AppUser>();
+            CreateMap<UpdateLibrarianDto, AppUser>();
+            CreateMap<AppUser, GetAllLibrarianDto>();
+            CreateMap<AppUser, GetLibrarianDto>();
         }
     }
 }
