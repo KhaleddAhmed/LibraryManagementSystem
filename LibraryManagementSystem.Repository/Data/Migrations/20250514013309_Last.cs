624a645733264d0afaf73f5f913d569ad7c314f2
khaledahmed2 <khaled.ahmed@appenza-studio.com>
2025-05-14 04:33:52 +0300

    Latest

---
+++ LibraryManagementSystem.Repository/Data/Migrations/20250514013309_Last.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.Repository.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Last : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnedDate",
+                table: "UserBorrowings",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AddColumn<double>(
+                name: "BorrowPricePerDay",
+                table: "Books",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BorrowPricePerDay",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReturnedDate",
+                table: "UserBorrowings",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+    }
+}
